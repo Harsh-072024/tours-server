ffb7b36a1d41e1c20376b3fbdad8cf3507d436e8
raushan <rk0383944@gmail.com>
2025-01-10 12:53:06 +0530

    file changes

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ dotenv.config()
 const app = express();
 const port = process.env.PORT  || 8000;
 const corsOptions = {
-    origin:true,
+    origin: ["https://tours-client-kappa.vercel.app"],
+    methods:["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }
 
